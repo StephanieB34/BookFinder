17fce58147096110aa04fd1266aad4eeb93672a6
Stephanie Beres <stephanie.beres34@gmail.com>
2018-12-07 08:43:35 -0700

    "added footer"

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const nytimesKey = " 478e0a6fb75449b986daf400d6d72c50"
+const googleBooksKey = "AIzaSyCMWQfZEffiicT5FNa2Dx_PUYdVLpaJBW4"
+
+//---loading page- hide results and details page
+$(document).ready(function (){
+  $('.results').hide();
+  $('.details').hide();
+});
+
+
+//submit function -drop down
+//api key and url
+//fetch/get request function-hide landing page, show results page
+
+//---results page
+  //function-showData
+  //event listener-buy from Amazon
+  //event listener-details button
+
+//details-page
+  //function-renderResults
+  //event listener-buy from Amazon
+  //event listener-Search again
