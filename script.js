6aab901858397b68c46bd073014f9e5f9ce4c28e
Stephanie Beres <stephanie.beres34@gmail.com>
2018-12-11 09:37:20 -0700

    fixed url and form id

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const nytimesKey = " 478e0a6fb75449b986daf400d6d72c50"
-var  searchURL = "https://api.nytimes.com/svc/books/v3/lists.json"
-const googleBooksKey = "AIzaSyCMWQfZEffiicT5FNa2Dx_PUYdVLpaJBW4"
+var apiKey = "478e0a6fb75449b986daf400d6d72c50"
+
+var  searchURL = `https://api.nytimes.com/svc/books/v3/lists.json?api-key=${apiKey}`
+//const googleBooksKey = "AIzaSyCMWQfZEffiicT5FNa2Dx_PUYdVLpaJBW4"
 
 //---loading page- hide results and details page
 $(document).ready(function (){
@@ -10,17 +11,19 @@ $(document).ready(function (){
   $('.details').hide();
 });
 
-$('#action.page').submit(function (event) {
+$('#list-form').submit(function (event) {
   event.preventDefault();
   let selectedList = $("#list").val();
 
   getDataFromAPI(selectedList);
 });
 
+
+
 function getDataFromAPI(selectedList) {
-  fetch (searchURL)
+  var url = `${searchURL}&list=${selectedList}`
+  fetch (url, {method: 'GET'})
   .then(response => {
-    console.log(response);
     if (response.ok) {
       return response.json();
     }
@@ -33,3 +36,26 @@ function getDataFromAPI(selectedList) {
   });
 }
 
+function displayResults(responseJson) {
+  console.log(responseJson);
+  // $('#results').show();
+  // $('#action.page').hide();
+  // for(let i= 0; i< responseJson.items.length; i++) {
+
+  //   $('#results-list').append (
+  //     `<li><h3>${responseJson.items[i].book.title}</h3>
+  //     <p>${responseJson.items[i].book.author}</p>
+  //     <p>${responseJson.items[i].book.isnb}</p>
+  //     </li>`
+  //   )
+  // };
+  
+  // $('#results').removeClass('hidden');
+}
+
+function getRequest(term, lists) {
+  $('#list-form').hide();
+  $('#results').show();
+
+  
+}
