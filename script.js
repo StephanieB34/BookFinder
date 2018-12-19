1acd63da915bb0e0a001ebad582bd7799d91e5a3
Stephanie Beres <stephanie.beres34@gmail.com>
2018-12-19 09:54:32 -0700

    all pages working

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 "use strict";
 
 var apiKey = "478e0a6fb75449b986daf400d6d72c50";
-
+var googleBooksKey="AIzaSyDGU-FxJ-Dl4ax8JmNmzTwpImlD_x75OMw";
 var searchURL = "https://api.nytimes.com/svc/books/v3/lists.json";
-//const googleBooksKey = "AIzaSyCMWQfZEffiicT5FNa2Dx_PUYdVLpaJBW4"
+var defaultImg ="https://s3-us-west-2.amazonaws.com/s.cdpn.io/387928/book%20placeholder.png"
 var bookArray = [];
 
 //---loading page- hide results and details page
@@ -85,25 +85,51 @@ function formatQueryParams(params) {
 function displayResults(bookArray) {
   $("#results-list").empty();
   for (let i = 0; i < bookArray.length; i++) {
+  if(bookArray[i].isbns && bookArray[i].isbns.length > 0) {
+    var isbn = bookArray[i].isbns[0].isbn10;
     $("#results-list").append(
       `<li>
-        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/387928/book%20placeholder.png" class="book-cover">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/387928/book%20placeholder.png" class="book-cover" id="cover-${isbn}">
         <h3>${bookArray[i].book_details[0].title}</h3>
         <p>${bookArray[i].book_details[0].author}</p>
-        <a href="${bookArray[i].amazon_product_url}">Buy</a>
-        <button data-index="${i}" class="details-button">details</button>
+        <a href="${bookArray[i].amazon_product_url}" class="button">Buy</a>
+        <button data-index="${i}" class="details-button button">details</button>
         <div class="clear"></div>
        </li>`
     );
+    
+    updateCover(isbn,i);
+  }
   }
 }
+
+function updateCover(isbn,index) {
+  fetch('https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn + "&key=" + googleBooksKey, {
+    method: 'get'
+  })
+  .then(response => { return response.json(); })
+  .then(data => {
+    var img = data.items? data.items[0].volumeInfo.imageLinks.thumbnail : defaultImg; 
+    img = img.replace(/^http:\/\//i, 'https://');
+    $('#cover-' + isbn).attr('src', img);
+    bookArray[index].image=img
+  })
+  .catch(error => {
+    console.log(error);
+    console.log('Googel API Error: Defaulting to archival images for book' + ' ISBN: ' + isbn);
+    /*var index = id - 1;
+    var img = archivedImages[index];
+    $('#cover-' + id).attr('src', img);*/
+  });
+}
+
 
 function displayDetails(book) {
   console.log("Details for book:", book);
   $(".details-container").empty();
   $(".details-container").append(
     `<div>
-    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/387928/book%20placeholder.png" class="book-cover">
+    <img src="${book.image||defaultImg}" class="book-cover">
        <h3>${book.book_details[0].title}</h3>
        <p>${book.book_details[0].description}</p>
        <p>${book.book_details[0].author}</p>
